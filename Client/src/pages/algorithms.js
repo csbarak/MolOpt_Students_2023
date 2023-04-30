f01ef02faa75524c009fa98d3f5a68f937663b47
Daniel Piro <danielpiro2@gmail.com>
2023-04-30 19:10:36 +0300

    added redirect when user not logged in

--- Client/src/pages/algorithms.js
+++ Client/src/pages/algorithms.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
 import AlgorithmsCard from 'src/components/algorithms-card'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useCookies } from 'react-cookie'
 
 const Algorithms = () => {
+  const [cookies, setCookie, removeCookie] = useCookies()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      cookies === undefined ||
+      cookies.email === undefined ||
+      cookies.token === undefined ||
+      cookies.email === '' ||
+      cookies.token === '' ||
+      cookies.email === null ||
+      cookies.token === null
+    ) {
+      alert('You are not logged in')
+      return router.push('/login')
+    }
+  }, [])
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
--- Client/src/pages/user-managment.js
+++ Client/src/pages/user-managment.js
@@ -1,14 +1,33 @@
 // ** React Imports
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import React from 'react'
 // ** MUI Imports
 import { Card, CardContent } from '@mui/material'
 import { TabPanel, TabContext } from '@mui/lab'
 import AdminUsers from 'src/components/admin-users'
+import { useCookies } from 'react-cookie'
+import { useRouter } from 'next/router'
 
 const UserManagment = () => {
   // ** State
   const [value, setValue] = useState('1')
+  const [cookies, setCookie, removeCookie] = useCookies()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      cookies === undefined ||
+      cookies.email === undefined ||
+      cookies.token === undefined ||
+      cookies.email === '' ||
+      cookies.token === '' ||
+      cookies.email === null ||
+      cookies.token === null
+    ) {
+      alert('You are not logged in')
+      return router.push('/login')
+    }
+  }, [])
 
   return (
     <Card>
