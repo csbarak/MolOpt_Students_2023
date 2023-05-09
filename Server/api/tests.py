764ccba962586451fa8e32713f74388fe9643523
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2023-05-09 12:28:59 +0300

    Adding unit tests for backend

--- Server/api/tests.py
+++ Server/api/tests.py
@@ -1,3 +1,155 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+#import logging
+# from api import views
+from .models import UserProfile as User
+from .models import UserAlgoritmRun
 
-# Create your tests here.
+### ======================== TEST VARIABLES ======================== ### 
+api_path = '/api/'      # Full path: 'http://localhost:8000/api/'
+### ================================================================ ###
+
+class RegisterTestCase(TestCase):
+    def setUp(self):
+        # insert user's registration data
+        self.email = 'testuser@gmail.com'
+        self.first_name = 'testing'
+        self.last_name = 'user'
+        self.affiliation = 'None'
+        self.position = 'None'
+        self.password = 'Password123'
+
+    def test_successful_registration(self):
+        url = api_path+'users/'
+        data = {'email': self.email, 'password': self.password, 'first_name': self.first_name,
+                'last_name': self.last_name, 'affiliation': self.affiliation, 'position': self.position}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)   # check that the new user has been created
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        # registering a fake user for the test
+        self.email = 'testuser@gmail.com'
+        self.first_name = 'testing'
+        self.last_name = 'user'
+        self.affiliation = 'None'
+        self.position = 'None'
+        self.password = 'Password123'
+        self.user = User.objects.create_user(self.email, self.first_name, self.last_name, self.affiliation, self.position, self.password)
+
+    def test_successful_login(self):
+        url = api_path+'login/'
+        data = {'username': self.email, 'password': self.password}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+        token = Token.objects.get(user=self.user)
+        self.assertEqual(response.data['token'], token.key)
+
+    def test_invalid_username(self):
+        response = self.client.post(reverse('login'), {'username': 'invalid', 'password': self.password})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_password(self):
+        response = self.client.post(reverse('login'), {'username': self.email, 'password': 'invalid'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutTestCase(TestCase):
+    def setUp(self):
+        # registering a fake user for the test
+        self.email = 'testuser@gmail.com'
+        self.first_name = 'testing'
+        self.last_name = 'user'
+        self.affiliation = 'None'
+        self.position = 'None'
+        self.password = 'Password123'
+        self.user = User.objects.create_user(self.email, self.first_name, self.last_name, self.affiliation, self.position, self.password)
+
+    def test_successful_logout(self):
+        url = api_path+'logout/'
+        self.client.login(username=self.email, password=self.password)
+        response = self.client.post(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+# Getters tests
+class AuthenticatedViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.email = 'testuser@gmail.com'
+        self.first_name = 'testing'
+        self.last_name = 'user'
+        self.affiliation = 'None'
+        self.position = 'None'
+        self.password = 'Password123'
+        self.user = User.objects.create_user(self.email, self.first_name, self.last_name, self.affiliation, self.position, self.password)
+        self.client.login(username=self.email, password=self.password)
+
+
+    def test_get_user(self):
+        url = reverse('get_user')
+        response = self.client.post(url, {'email': self.email})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        # validate user gotten
+        self.assertEqual(response.json()['id'], 1)  # only one user was created for this test
+        self.assertEqual(response.json()['email'], self.email)
+        self.assertEqual(response.json()['first_name'], self.first_name)
+        self.assertEqual(response.json()['last_name'], self.last_name)
+        self.assertEqual(response.json()['affiliation'], self.affiliation)
+        self.assertEqual(response.json()['position'], self.position)
+        self.assertEqual(response.json()['is_staff'], False)
+    
+
+    def test_get_user_tasks(self):
+        # create a task
+        algo_name = 'Machine Learning'
+        algo_status = 'running'
+        my_run = UserAlgoritmRun()
+        my_run.user_email = self.email
+        my_run.status = algo_status
+        my_run.algorithm_name = algo_name
+        my_run.save()
+
+        url = reverse('get_user_runs')
+        response = self.client.post(url, {'email': self.email})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+        # validate task gotten
+        self.assertEqual(response.json()[0]['id'], 1)  # only one task was created for this test
+        self.assertEqual(response.json()[0]['user_email'], self.email)
+        self.assertEqual(response.json()[0]['algorithm_name'], algo_name)
+        self.assertEqual(response.json()[0]['status'], algo_status)
+        # self.assertEqual(response.json()[0]['time'], '')
+        # self.assertEqual(response.json()[0]['result'], '')
+    
+
+    def test_get_permission_level(self):
+        url = reverse('check_permission')
+        response = self.client.post(url, {'email': self.email})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['is_admin'], False)
+
+
+    ## TODO: Needs to add an admin user for these test!
+    ## ================================================
+    
+    # def test_get_all_users(self):
+    #     url = reverse('get_all_users')
+    #     response = self.client.post(url, {'email': self.email})
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(len(response.json()), 1)
+    
+
+    # def test_get_all_tasks(self):
+    #     task = Task.objects.create(title='Test Task', description='Test Description', assignee=self.user)
+    #     url = reverse('get_all_tasks')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(len(response.json()), 1)
--- Server/api/urls.py
+++ Server/api/urls.py
@@ -3,51 +3,40 @@ from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
 from api import views
 
-
 router = DefaultRouter()
-# /api/users - shows all users
-# /api/users/user_id - shows user
-# /api/runs - shows all runs
-# /api/users/user_id - shows user's runs
+
 router.register('users', views.UserProfileViewSet)
 router.register('runs', views.UserRunsViewSet) 
 
-
 urlpatterns = [
-    ### post:
-    # path('users/',                      views.UserProfileViewSet),                           # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('get_user/',                   views.getUser.as_view()),                           # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('get_all_users/',              views.getAllUsers.as_view()),                       # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('check_permissions/',          views.CheckPermissions.as_view()),                  # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('create_admin/',               views.CreateSystemAdminApiView.as_view()),          # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('remove_admin/',               views.RemoveSystemAdminApiView.as_view()),          # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('delete_user/',                views.DeleteUserFromSystem.as_view()),              # body of the api is all the new user birthdate, phone, country, languages, gender.
-    path('run_alignment/',              views.UserRunAlignmentApiView.as_view()),           # body of the api is 2 files
-    path('run_feature/',                views.UserRunFeatureExtractionApiView.as_view()),   # body of the api is 1 file and list of packages to run
-    path('run_ML_algorithms/',          views.UserRunMLAlgorithmsApiView.as_view()),        # body of the api is 1 file and list of dictionaries (dict foreach alg)
-    path('get_all_runs/',               views.UserGetAllRunsApiView.as_view()),
-    path('get_user_runs/',              views.UserGetUserRunsApiView.as_view()),
-    path('remove_run/',                 views.UserRemoveRunApiView.as_view()),
-    path('download_result/',            views.UserDownloadResultApiView.as_view()),
-    path('run_auto_process/',           views.UserRunAutoProcessApiView.as_view()),
+    # path('users/',                      views.UserProfileViewSet),  # Gets all the users in the ssytem - Django default                         
+    path('login/',                      views.UserLoginApiView.as_view(),                       name="login"), 
+    path('logout/',                     views.UserLogoutApiView.as_view(),                      name="logout"),
+    path('contact-admin/',              views.ContactAdmin.as_view(),                           name="contact_admin"), 
+    path('create_admin/',               views.CreateSystemAdminApiView.as_view(),               name="create_admin"),          
+    path('remove_admin/',               views.RemoveSystemAdminApiView.as_view(),               name="remove_admin"),          
+    path('delete_user/',                views.DeleteUserFromSystem.as_view(),                   name="delete_user"),              
+    path('remove_run/',                 views.UserRemoveRunApiView.as_view(),                   name="remove_run"),  
+    
+    ### DATA GETTERS
+    path('get_user/',                   views.getUser.as_view(),                                name="get_user"),                           
+    path('get_all_users/',              views.getAllUsers.as_view(),                            name="get_all_users"),                       
+    path('get_all_runs/',               views.UserGetAllRunsApiView.as_view(),                  name="get_all_runs"),
+    path('get_user_runs/',              views.UserGetUserRunsApiView.as_view(),                 name="get_user_runs"),
+    path('check_permissions/',          views.CheckPermissions.as_view(),                       name="check_permission"),                  
 
-    
-    ### get:
-    # path('tasks/',                    views.TasksApiView.as_view()),
-    # path('users/',                    views.UsersApiView.as_view()),
-    # path('admin_inbox/',              views.AdminInboxApiView.as_view()),
-    
-    
+    ### ALGORITHMS
+    path('run_alignment/',              views.UserRunAlignmentApiView.as_view(),                name="align_alg"),           
+    path('run_feature/',                views.UserRunFeatureExtractionApiView.as_view(),        name="fe_alg"),
+    path('run_ML_algorithms/',          views.UserRunMLAlgorithmsApiView.as_view(),             name="ml_alg"),        
+    path('run_auto_process/',           views.UserRunAutoProcessApiView.as_view(),              name="auto_alg"),
+    path('download_result/',            views.UserDownloadResultApiView.as_view(),              name="download_result"),
+
     ### RESET PASSWORDS:
-    path('reset_password/',             auth_views.PasswordResetView.as_view(),            name="reset_password"),               # reset_password
-    path('reset_password_sent/',        auth_views.PasswordResetDoneView.as_view(),        name="password_reset_done"),          # reset_password
-    path('reset/<uidb64>/<token>/',     auth_views.PasswordResetConfirmView.as_view(),     name="password_reset_confirm"),       # reset_password
-    path('reset_password_complete/',    auth_views.PasswordResetConfirmView.as_view(),     name="password_reset_complete"),      # reset_password
+    path('reset_password/',             auth_views.PasswordResetView.as_view(),                 name="reset_password"),
+    path('reset_password_sent/',        auth_views.PasswordResetDoneView.as_view(),             name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',     auth_views.PasswordResetConfirmView.as_view(),          name="password_reset_confirm"),
+    path('reset_password_complete/',    auth_views.PasswordResetConfirmView.as_view(),          name="password_reset_complete"),
     
-    
-    
-    path('contact-admin/', views.ContactAdmin.as_view()), # body of the api is the user email and password
-    path('login/', views.UserLoginApiView.as_view()), # body of the api is the user email and password
-    path('logout/', views.UserLogoutApiView.as_view()),
     path('', include(router.urls))
 ]
