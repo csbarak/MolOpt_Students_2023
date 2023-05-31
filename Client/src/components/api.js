323ab995d92913648b8c4b218fb42171bae85788
Daniel Piro <danielpiro2@gmail.com>
2023-05-31 12:08:04 +0300

    Update api.js

--- Client/src/components/api.js
+++ Client/src/components/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
-//chnage 132.72.8.188 to localhost to run locally
+
+const dev = 'http://localhost:8000/api/'
+const prod = 'https://132.72.8.188:8000/api/'
+
 const api = axios.create({
-  baseURL: `http://132.72.8.188:8000/api/`
+  baseURL: prod
 })
 
 export default api
