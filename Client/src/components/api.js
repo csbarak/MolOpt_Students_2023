95bfa0fd9270e3c703f827f7380dd39d10a91e5a
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2023-06-04 15:02:37 +0300

    Finalize All Project Documents

--- Client/src/components/api.js
+++ Client/src/components/api.js
@@ -3,8 +3,12 @@ import axios from 'axios'
 const dev = 'http://localhost:8000/api/'
 const prod = 'https://132.72.8.188:8000/api/'
 
+// Note: use 'dev' to run MolOpt locally
+//       use 'prod' to run MolOpt remotely
+
 const api = axios.create({
-  baseURL: prod
+  baseURL: dev 
+  // baseURL: prod
 })
 
 export default api
